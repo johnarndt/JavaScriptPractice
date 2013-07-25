cd54c562c7362e42dd68e50f83e54cfeff8d3d51
John Arndt <johnrarndt@gmail.com>
2013-07-24 18:15:08 -0700

    Updated with comments and explanations

--- ConfirmingMyself.js
+++ ConfirmingMyself.js
@@ -12,15 +12,15 @@
 function myFunction()
 {
 var x;
-
+//Shows promt to enter name
 var person=prompt("Please enter your name");
-
+//Must enter exact name of John Arndt
 if (person == "John Arndt")
   {
   x="Hello John! How are you today?";
   document.getElementById("John").innerHTML=x;
   }
-
+//Anything entered other than John Arndt 
 if (person != "John Arndt")
   {
   x="You are not John you fool!!";
